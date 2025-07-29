7c7ee9cba85ef264a9357a66e33a78743b38a355
vicenteprogrammer <vicentejunior4522@gmail.com>
2025-07-29 08:44:38 -0300

    Add cors

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,10 @@
 import express, { json } from "express";
 import playRoutes from "./routes/players-route";
+import cors from 'cors';
 
 function createApp(){
     const app = express();
-
+    app.use(cors())
     app.use(json());
     
     app.use('/api', playRoutes)
